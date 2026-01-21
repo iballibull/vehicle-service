c76fa9d9c0876ad29cf81873ca949604772e53ab
iballibull <muchammadiqbalyusuf@gmail.com>
2026-01-21 14:05:05 +0700

    feat(errors): add custom error classes

---
+++ src/errors/response.error.js
@@ -0,0 +1,9 @@
+class ResponseError extends Error {
+  constructor(status, message, errors = {}) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+}
+
+export { ResponseError };
