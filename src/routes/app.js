a57ba447faf9cd39f686abf1f1a531adb81bcaba
iballibull <muchammadiqbalyusuf@gmail.com>
2026-01-22 15:32:48 +0700

    feat: add OpenAPI documentation and Swagger UI integration

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes.js';
 import { errorMiddleware } from '../middlewares/error.middleware.js';
+import openApiDocument from '../docs/openapi.json' with { type: 'json' };
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 app.use('/api', routes);
 app.use(errorMiddleware);
 
